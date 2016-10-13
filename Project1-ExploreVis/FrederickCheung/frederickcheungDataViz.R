a2385c76290494c5b8277b4ddcb757999c5771a6
ggionx <fred@frederickcheung.com>
2016-10-13 19:03:01 -0400

    having trouble with returning the max and min values for time stamps after grouping my categories

--- Project1-ExploreVis/FrederickCheung/frederickcheungDataViz.R
+++ Project1-ExploreVis/FrederickCheung/frederickcheungDataViz.R
@@ -6,7 +6,7 @@
 
 
 #set working directory and load packages
-setwd("Projects_NYCDSA7/DataViz")
+setwd("~/Dropbox/Projects_NYCDSA7/DataViz")
 library(dplyr)
 library(ggplot2)
 
@@ -39,17 +39,47 @@ newsplot <- ggplot(data = MergedNewsClean,aes(x=category))
 newsplot + geom_bar(aes(fill=category)) #is it possible to change the category labels directly in ggplot2?
 
 #Set each category equal to a variable
-#arrange, then return top cluster id 
+BusinessCluster <- MergedNewsClean %>% filter(., category == "business")
+EntertainmentCluster <- MergedNewsClean %>% filter(., category == "entertainment")
+HealthCluster <- MergedNewsClean %>% filter(., category == "health")
+TechnologyCluster <- MergedNewsClean %>% filter(., category == "technology")
 
-#Find difftime for max and min of the top story for each category
+#Find top stories in each category
+BusinessDf <- as.data.frame(table(BusinessCluster$clusterid))
+TopFrqBusiness <- BusinessDf[which.max(BusinessDf$Freq),]
+
+EntertainmentDf <- as.data.frame(table(EntertainmentCluster$clusterid))
+TopFrqEntertainment <- EntertainmentDf[which.max(EntertainmentDf$Freq),]
+
+HealthDf <- as.data.frame(table(HealthCluster$clusterid))
+TopFrqHealth <- HealthDf[which.max(HealthDf$Freq),]
+
+TechnologyDf <- as.data.frame(table(TechnologyCluster$clusterid))
+TopFrqTechnology <- TechnologyDf[which.max(TechnologyDf$Freq),]
+
+#filter Cluster variables based on Top clusterid
+TopStoryBusinessCluster <- MergedNewsClean %>% filter(., clusterid == "d2OyTeAXDFQpb3M9Cr_Ftde6Ig0aM")
+TopStoryEntertainmentCluster <- MergedNewsClean %>% filter(., clusterid == "d--MozT4MsoFfIMgKu5_N58OF_f9M")
+TopStoryHealthCluster <- MergedNewsClean %>% filter(., clusterid == "dRL3APAAYdKPwuMnih--zAQtflluM")
+TopStoryTechnologyCluster <- MergedNewsClean %>% filter(., clusterid == "dubwcJArLL_qAKML5LGPLiunKzNLM")
+
+# return min and max on time stamps for
+FirstTopStoryBusinessCluster <- TopStoryBusinessCluster %>% max(., timestamp, na.rm = FALSE)
+LastTopStoryBusinessCluster <- TopStoryBusinessCluster %>% min(., timestamp, na.rm = FALSE)
+FirstTopStoryEntertainmentCluster <- TopStoryEntertainmentCluster %>% max(., timestamp, na.rm = FALSE)
+LastTopStoryEntertainmentCluster <- TopStoryEntertainmentCluster %>% max(., timestamp, na.rm = FALSE)
+FirstTopStoryHealthCluster <- TopStoryHealthCluster %>% max(., timestamp, na.rm = FALSE)
+LastTopStoryHealthCluster <- TopStoryHealthCluster %>% max(., timestamp, na.rm = FALSE)
+FirstTopStoryTechnologyCluster <- TopStoryTechnologyCluster %>% max(., timestamp, na.rm = FALSE)
+LastTopStoryTechnologyCluster <- TopStoryTechnologyCluster %>% max(., timestamp, na.rm = FALSE)
 
 #Sum categories
 #CategoryCount <- data.frame(table(MergedNews$category))
 
 #Top clusterid
-TopStories <- head(unique(MergedNews$clusterid))
+#TopStories <- head(unique(MergedNews$clusterid))
 
 #Top hdline via clusterid
 
 #Top 10 publishers
-Top10Publishers <- head(MergedNews$publisher, n=10)                                
+# Top10Publishers <- head(MergedNews$publisher, n=10)                                
