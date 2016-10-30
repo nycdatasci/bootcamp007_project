41bb9f47aebdfd71574a7a63d6c0dac431417dd2
ggionx <fred@frederickcheung.com>
2016-10-29 23:56:50 -0400

    cannot remove X from names vectors

--- Project2-Shiny/frederickcheung_Shiny/pivotHealthdata.R
+++ Project2-Shiny/frederickcheung_Shiny/pivotHealthdata.R
@@ -1,6 +1,8 @@
-library(reshape)
+healthdf2 <- healthdf[,c(1,62:63,5:61)]
+
+healthreshape <- as.data.frame(t(healthdf2))
+
+gsub('X', '', healthreshape[, "49"])
+years = gsub('X', '', names(healthreshape[, "49"]))
 
 
-
-healthreshape <- as.data.frame(t(healthdf))
-healthreshape
