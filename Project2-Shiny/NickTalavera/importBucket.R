790f61db04ec1fd3fcec4419a28260087a1a6315
NickTalavera <contact@nicktalavera.com>
2016-10-30 19:55:31 -0400

    submit

---
+++ Project2-Shiny/NickTalavera/importBucket.R
@@ -0,0 +1,9 @@
+# library("aws.s3")
+# bucketname = 'datanicktalavera'
+# Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAJIM6UVWCQSV4WHDA",
+#            "AWS_SECRET_ACCESS_KEY" = "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")
+# bucketlist()
+# get_bucket(bucket = bucketname)
+# # s3load("DNMData.csv", bucket = bucketname)
+# bucket_exists(bucketname)
+
