f926513cb83583f3cd457b096daa148d3c32c906
ggionx <fred@frederickcheung.com>
2016-10-30 16:12:15 -0400

    this basic project works, used GoogleVis

--- Project2-Shiny/frederickcheung_Shiny/pivotHealthdata.R
+++
@@ -1,8 +0,0 @@
-healthdf2 <- healthdf[,c(1,62:63,5:61)]
-
-healthreshape <- as.data.frame(t(healthdf2))
-
-gsub('X', '', healthreshape[, "49"])
-years = gsub('X', '', names(healthreshape[, "49"]))
-
-
