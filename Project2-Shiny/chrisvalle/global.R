b1506967661bb1ca3c207dbfdb9717e707c43dbc
chrisanalytics <chrisqurious@gmail.com>
2016-10-30 17:50:10 -0400

    proj2

---
+++ Project2-Shiny/chrisvalle/global.R
@@ -0,0 +1,4 @@
+#load dataset
+sbuxRevFY = read.csv(file = "/Users/chris/Desktop/shinySbux/data/sbuxRevFY.csv")
+location = read.csv(file ="/Users/chris/Desktop/shinySbux/data/All_Starbucks_Locations_in_the_World.csv")
+#storeTxn= read.xlsx(file = "/Users/chris/Desktop/shinySbux/data/storeTxn.xlsx")
