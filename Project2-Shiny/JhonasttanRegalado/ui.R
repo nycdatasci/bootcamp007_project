38adef93ef35afe019f626747e6fefef99fdce63
jhonasttan <jhonasttan@gmail.com>
2016-10-30 23:48:23 -0400

    updated info section

--- Project2-Shiny/JhonasttanRegalado/ui.R
+++ Project2-Shiny/JhonasttanRegalado/ui.R
@@ -64,12 +64,17 @@ shinyUI(dashboardPage(
                        column(12, box(htmlOutput("gaugeDocks"), height = 30000,width = 18)))),
       tabItem(tabName = "introVideo",
               fluidRow(column(12, div(align = "left", HTML("<br><h2>Analyze NYC Citi Bike Bike / Station Capacity and Plan your Trip<h2><br>"))),
-                       column(6, div(align = "left", HTML("<p><h3>I have been fascinated by the number of people riding NYC Citi Bikes to and from work. 
+                       column(8, div(align = "left", HTML("<p><h3>The Citibike Station Capacity Analysis working prototype is a result of project 2 of the NYC Data Science Academy <a href='http://nycdatascience.com/data-science-bootcamp/'>boot camp</a>
+                                                            (solve a business problem using open source application development frameworks Shiny and RStudio and Open Data). I have been fascinated by the number of people riding NYC Citi Bikes for either commuting or recreation. 
                                                            I’ve asked a few Citi Bike subscribers about their experience, and yes, some have dealt with a pothole or two; therefore, was interested in understanding the logistics. 
                                                            How are the stations replenished and how do users typically deal with bike capacity constraints when they really need a bike. 
                                                            Do they go to a nearby station, or hail a cab? 
-                                                          The Citibike Station Capacity Analysis product was developed and implemented within a two-week timeline using the Lean Startup methodology. 
-                                                          The goal of this working prototype is to help users address the identified gaps. I welcome you to try the product and 
+                                                          <br><br><p>This concept was developed and implemented within a two-week timeline using the Lean Startup and Project Management methodologies, 
+                                                          Agile Development principles, DevOps practices, Open Source technology, Cloud based infrastructure and Open Data. 
+                                                          The effort has led to a modified goal. To use this working prototype as a concept that introduces web site feature enrichments to the end user experience during their daily interaction with the
+                                                          the NYC Citibike Station Map <a href='https://member.citibikenyc.com/map/'>website</a>.
+                                                          The proposed ideas could be integrated into their product development pipeline. 
+                                                          <br><br><p>I welcome you to try the product and 
                                                           provide constructive feedback. Thanks!</p>"))),
                        column(12, div(align = "left", HTML("<br><h2>How to Video<h2><br>"))),
                        column(12, div(align = "left", embed_youtube(id = "StMAiYCMuco", frameborder = 50))),
@@ -85,7 +90,8 @@ shinyUI(dashboardPage(
                                       
                             )
                           )),
-                       column(12, div(align = "left", HTML("<br><h3><b>Feedback<b>: <a href='mailto:jhonasttan@gmail.com?Subject=[Shiny%20App%20Feedback]%20NYC%20Citi%20Bike%20Availability%20Analysis' target='_top'>Jhonasttan Regalado, PMP</a><h3>")))
+                       column(12, div(align = "left", HTML("<br><h3><b>Feedback<b>: <a href='mailto:jhonasttan@gmail.com?Subject=[Shiny%20App%20Feedback]%20NYC%20Citi%20Bike%20Availability%20Analysis' target='_top'>Constructive feedback is welcomed!</a><h3>"))),
+                       column(12, div(align = "left", HTML("<br><h3><b>LinkedIn Profile<b>: <a href='https://www.linkedin.com/in/jhonasttanregalado'>Jhonasttan Regalado, PMP</a><h3>")))
                        ))
       
       ))
