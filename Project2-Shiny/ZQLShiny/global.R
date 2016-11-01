0edaf46e2c85afae53d1f32c95e9637ac8db6146
CherylLiu2016 <ziqiaoliu8709@gmail.com>
2016-10-31 23:02:26 -0400

    update files

--- Project2-Shiny/ZQLShiny/global.R
+++ Project2-Shiny/ZQLShiny/global.R
@@ -1,3 +1,24 @@
 
 #use wide dataframe
-data <- read.csv('shiny_wide95.csv', stringsAsFactors = F)
+data <- read.csv('shiny_wide.csv', stringsAsFactors = F)
+data_long <- read.csv('shiny_long.csv', stringsAsFactors = F)
+
+trend_data <- subset(data_long,CountryName%in%c("United States","United Kingdom",
+                                                "United Arab Emirates", "Spain",
+                                                "Sweden","New Zealand",
+                                                "Ireland","Israel","Italy",
+                                                "Japan", "Mexico","Netherlands",
+                                                "India","France", "Germany",
+                                                "Canada","Chile","China",
+                                                "Colombia", "Australia", "Brazil") )
+trend_cn <- c("United States","United Kingdom",
+              "United Arab Emirates", "Spain",
+              "Sweden","New Zealand",
+              "Ireland","Israel","Italy",
+              "Japan", "Mexico","Netherlands",
+              "India","France", "Germany",
+              "Canada","Chile","China",
+              "Colombia", "Australia", "Brazil")
+
+
+
