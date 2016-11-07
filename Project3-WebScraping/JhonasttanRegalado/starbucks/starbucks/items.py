6bbf4ff1c1154f3804a5f72d6c2e179d67a6184f
jhonasttan <jhonasttan@gmail.com>
2016-11-07 12:44:26 -0500

    working version

--- Project3-WebScraping/JhonasttanRegalado/starbucks/starbucks/items.py
+++ Project3-WebScraping/JhonasttanRegalado/starbucks/starbucks/items.py
@@ -5,10 +5,16 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+#import scrapy
+from scrapy import Item, Field
 
-
-class StarbucksItem(scrapy.Item):
+class StarbucksItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    StoreId = Field(serializer=str)
+    Name = Field(serializer=str)
+    PhoneNumber = Field(serializer=str)
+    Coordinates = Field(serializer=str)
+    Address = Field(serializer=str)
+    Features = Field(serializer=str)
+    Slug = Field(serializer=str)
--- Project3-WebScraping/JhonasttanRegalado/starbucks/starbucks/pipelines.py
+++ Project3-WebScraping/JhonasttanRegalado/starbucks/starbucks/pipelines.py
@@ -5,7 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exporters import CsvItemExporter
 
-class StarbucksPipeline(object):
+
+class WriteItemPipeline(object):
+        
+    def __init__(self):
+            self.filename = 'StarbucksStores.txt'
+
+    def open_spider(self, spider):
+            self.csvfile = open(self.filename, 'wb')
+            self.exporter = CsvItemExporter(self.csvfile)
+            self.exporter.start_exporting()
+            
+    def close_spider(self, spider):
+            self.exporter.finish_exporting()
+            self.csvfile.close()
+
     def process_item(self, item, spider):
-        return item
+            self.exporter.export_item(item)
+            
+            return item
+
