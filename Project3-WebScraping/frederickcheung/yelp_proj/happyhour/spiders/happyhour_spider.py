aa6611c967b719d6d9f786a49dde0da06565781c
ggionx <fred@frederickcheung.com>
2016-11-07 21:27:46 -0500

    page number returns properly? doesn't parse listing contents

--- Project3-WebScraping/frederickcheung/yelp_proj/happyhour/spiders/happyhour_spider.py
+++ Project3-WebScraping/frederickcheung/yelp_proj/happyhour/spiders/happyhour_spider.py
@@ -3,26 +3,49 @@ from scrapy import Spider
 from scrapy.selector import Selector
 from happyhour.items import YelpItem
 
+#can throw into MongoDB
+
 class yelpSpider(scrapy.Spider):
     name = "happyhour_spider"
     allowed_domains = ['https://www.yelp.com']
     start_urls = ["https://www.yelp.com/search?find_desc=Restaurants&find_loc=Boston,+MA&start=0&sortby=rating&attrs=HappyHour"]
 
+    def last_pagenumber_in_search(self, response):
+        try:
+            last_page_number = int(re.search(r'start=(\d+)',response
+                .xpath('//*[@class="available-number pagination-links_anchor"]')
+                .extract()[-1])
+                .group(1))
+            return last_page_number    
+        except:
+            print response.xpath('//*[@class="available-number pagination-links_anchor"]').extract()
+    
     def parse(self, response):
+        last_page_number = self.last_pagenumber_in_search(response)
+        if last_page_number < 10:
+            return
+        else:
+            page_urls = [response.url + "?start=" + str(pageNumber)
+                for pageNumber in range(0, last_page_number+10, 10)]
+            for page_url in page_urls:
+                yield scrapy.Request(page_url,
+                                    callback=self.parse_listing_contents)    
+
+
+
+    def parse_listing_contents(self, response):
 
         from scrapy import Selector
-        
         full_listing = response.xpath('//*[@class="biz-listing-large"]').extract()
-        
-
         for listing in full_listing:  
             item = YelpItem()
             name = Selector(text=listing).xpath('//a[@class="biz-name js-analytics-click"]//span/text()').extract()
             item['name'] = name
-            address = ' '.join(Selector(text=listing).xpath('//address//text()').extract()).strip()
-            item['address'] = address
+            address = Selector(text=listing).xpath('//address//text()').extract()
+            item['address'] = ','.join(address).strip()
             url = Selector(text=listing).xpath('//a[@class="biz-name js-analytics-click"]/@href').extract()
-            item['url'] = url
+            item['url'] = 'www.yelp.com'+str(url)
+            #'www.yelp.com'+str(url[0])
             phone = Selector(text=listing).xpath('//*[@class="biz-phone"]//text()').extract()
             item['phone'] = phone
             # rating = 
@@ -32,12 +55,5 @@ class yelpSpider(scrapy.Spider):
 
             yield item
 
-        # for url in urls:
-        #     item = YelpItem()
-        #     item['url'] = url
-        #     yield item
 
-        # for name in names:
-        #     item = YelpItem()
-        #     item['name'] = name
-        #     yield item
+
