6920f572238f6e6e24e2bfe0717f2c82ab894036
NickTalavera <contact@nicktalavera.com>
2016-11-13 22:47:26 -0500

    Improved roundup

--- Project3-WebScraping/NickTalavera/XboxBackCompat/global.R
+++ Project3-WebScraping/NickTalavera/XboxBackCompat/global.R
@@ -33,7 +33,10 @@ moveMe <- function(data, tomove, where = "last", ba = NULL) {
   x
 }
 
-roundUp <- function(x) 10^ceiling(log10(x))
+roundUp <- <- function(x, nice=c(1,2,4,5,6,8,10)) {
+  if(length(x) != 1) stop("'x' must be of length 1")
+  10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
+}
 ###############################################################################
 #                         LOAD PACKAGES AND MODULES                          #
 ###############################################################################
--- Project3-WebScraping/NickTalavera/XboxBackCompat/ui.R
+++ Project3-WebScraping/NickTalavera/XboxBackCompat/ui.R
@@ -1,7 +1,10 @@
 # Xbox One Backwards Compatiablity Predictor
 
 library(shiny)
-roundUp <- function(x) 10^ceiling(log10(x))
+roundUp <- function(x, nice=c(1,2,4,5,6,8,10)) {
+  if(length(x) != 1) stop("'x' must be of length 1")
+  10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
+}
 
 programName = "Xbox One Backwards Compatibility Predictor"
 sideBarWidth = 450
