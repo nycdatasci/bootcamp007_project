be2341514591c0c5a09c371dea83c0e46e0e1576
NickTalavera <contact@nicktalavera.com>
2016-11-15 11:16:22 -0500

    Update markdown. Added functionality for multi search box

--- Project3-WebScraping/NickTalavera/server.R
+++ Project3-WebScraping/NickTalavera/server.R
@@ -65,7 +65,6 @@ shinyServer(function(input, output, session) {
     print(paste("SEARCH_Is_Backwards_Compatible", input$SEARCH_Is_Backwards_Compatible))
     print(paste("SEARCH_Predicted_to_become_Backwards_Compatible", input$SEARCH_Predicted_to_become_Backwards_Compatible))
     print(paste("SEARCH_Backwards_Compatability_Probability_Percent", input$SEARCH_Backwards_Compatability_Probability_Percent))
-    # print(paste("SEARCH_Release_date", input$SEARCH_Release_date))
     if (!is.null(input$SEARCH_Release_date)) {
       dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
     }
@@ -86,17 +85,42 @@ shinyServer(function(input, output, session) {
     print(paste("SEARCH_Metacritic_Review_Score", input$SEARCH_Metacritic_Review_Score))
     print(paste("SEARCH_Metacritic_User_Review_Score", input$SEARCH_Metacritic_User_Review_Score))
     print(paste("SEARCH_Price_on_Xbox.com", input$SEARCH_Price_on_Xbox.com))
-    print(paste("SEARCH_Publisher", input$SEARCH_Publisher))
-    print(paste("SEARCH_Developer", input$SEARCH_Developer))
-    print(paste("SEARCH_Genre", input$SEARCH_Genre))
-    print(paste("SEARCH_ESRB_Rating", input$SEARCH_ESRB_Rating))
-    print(paste("SEARCH_Features", input$SEARCH_Features))
+    if (!is.null(input$SEARCH_Publisher)) {
+      dataToDisplay = dataToDisplay[which((dataToDisplay$publisher%in%input$SEARCH_Publisher)),]
+    }
+    if (!is.null(input$SEARCH_Developer)) {
+      dataToDisplay = dataToDisplay[which((dataToDisplay$developer%in%input$SEARCH_Developer)),]
+    }
+    if (!is.null(input$SEARCH_Genre)) {
+      dataToDisplay = dataToDisplay[which((dataToDisplay$genre%in%input$SEARCH_Genre)),]
+    }
+    if (!is.null(input$SEARCH_ESRB_Rating)) {
+      dataToDisplay = dataToDisplay[which((dataToDisplay$ESRBRating%in%input$SEARCH_ESRB_Rating)),]
+    }
+    if (!is.null(input$SEARCH_Features)) {
+      dataToDisplay = dataToDisplay[which((dataToDisplay$features%in%input$SEARCH_Features)),]
+    }
     print(paste("SEARCH_Smartglass_Compatable", input$SEARCH_Smartglass_Compatable))
     print(paste("SEARCH_Number_of_Game_Add_Ons", input$SEARCH_Number_of_Game_Add_Ons))
+    if (!is.null(input$SEARCH_Release_date)) {
+      dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
+    }
     print(paste("SEARCH_Number_of_Avatar_Items", input$SEARCH_Number_of_Avatar_Items))
+    if (!is.null(input$SEARCH_Release_date)) {
+      dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
+    }
     print(paste("SEARCH_Number_of_GamerPics", input$SEARCH_Number_of_GamerPics))
+    if (!is.null(input$SEARCH_Release_date)) {
+      dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
+    }
     print(paste("SEARCH_Number_of_Themes", input$SEARCH_Number_of_Themes))
+    if (!is.null(input$SEARCH_Release_date)) {
+      dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
+    }
     print(paste("SEARCH_Number_of_Game_Videos", input$SEARCH_Number_of_Game_Videos))
+    if (!is.null(input$SEARCH_Release_date)) {
+      dataToDisplay = dataToDisplay[dataToDisplay$releaseDate >= input$SEARCH_Release_date[1] & dataToDisplay$releaseDate <= input$SEARCH_Release_date[2],]
+    }
     
     # if (!is.null(input$marketName)) {
     #   dataToDisplay = dataToDisplay[which((dataToDisplay$Market_Name%in%input$marketName)),]
@@ -105,9 +129,7 @@ shinyServer(function(input, output, session) {
     # if (!is.null(input$drugName)) {
     #   dataToDisplay = dataToDisplay[which((dataToDisplay$Drug_Type%in%input$drugName)),]
     # }
-    # if (!is.null(input$shippedFrom)) {
-    #   dataToDisplay = dataToDisplay[which((dataToDisplay$Shipped_From%in%input$shippedFrom)),]
-    # }
+
     # # switch(input$weightUnits)
     # if (!is.null(input$weightUnits)) {
     #   drugUnitMutiplier = c(1000,1e+6,1,0.001,NA,0.035274,0.0022046249999752, 0.00000110231131)
