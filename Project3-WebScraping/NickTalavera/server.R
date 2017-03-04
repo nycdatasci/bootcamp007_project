4e1c1a2ed06d47c34b648f79709826b2ee4b172f
NickTalavera <contact@nicktalavera.com>
2017-03-04 11:46:31 -0600

    Logical fixes

--- Project3-WebScraping/NickTalavera/server.R
+++ Project3-WebScraping/NickTalavera/server.R
@@ -148,8 +148,8 @@ shinyServer(function(input, output, session) {
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
         dataToPresent = dataToPresent[!is.na(dataToPresent$Name),]
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         print(nrow(dataToPresent))
         dataToPresent
       }, 
@@ -172,8 +172,8 @@ shinyServer(function(input, output, session) {
                                       "Kinect Required" = isKinectRequired, "Exclusive" = isExclusive, "Is Console Exclusive" = isConsoleExclusive, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       options = list(scrollX = TRUE,
@@ -193,8 +193,8 @@ shinyServer(function(input, output, session) {
                                       "Kinect Required" = isKinectRequired, "Exclusive" = isExclusive, "Is Console Exclusive" = isConsoleExclusive, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       options = list(scrollX = TRUE,
@@ -217,8 +217,8 @@ shinyServer(function(input, output, session) {
                                       "Kinect Required" = isKinectRequired, "Exclusive" = isExclusive, "Is Console Exclusive" = isConsoleExclusive, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = as.numeric(price), 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       options = list(scrollX = TRUE,
@@ -241,8 +241,8 @@ shinyServer(function(input, output, session) {
                                       "Exclusive" = isExclusive, "Is Console Exclusive" = isConsoleExclusive, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       options = list(scrollX = TRUE,
@@ -263,8 +263,8 @@ shinyServer(function(input, output, session) {
                                       "Kinect Required" = isKinectRequired, "Exclusive" = isExclusive, "Is Console Exclusive" = isConsoleExclusive, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       options = list(scrollX = TRUE,
@@ -283,8 +283,8 @@ shinyServer(function(input, output, session) {
                                       "Kinect Required" = isKinectRequired, "Metacritic Rating" = reviewScorePro, 
                                       "Metacritic User Rating" = reviewScoreUser, "Xbox User Rating" = xbox360Rating, 'Price' = price, 'Game Addons' = DLgameAddons, "Genre" = genre,
                                       'Publisher'= publisher, 'Developer' = developer, "Xbox One Version Available" = isOnXboxOne)
-        dataToPresent[dataToPresent == TRUE] = "Yes"
-        dataToPresent[dataToPresent == FALSE] = "No"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+        dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
         dataToPresent
       }, selection = "none",
       height = NULL,
@@ -307,8 +307,8 @@ shinyServer(function(input, output, session) {
       dataToPresent = dataToPresent[dataToPresent$isKinectRequired == FALSE & dataToPresent$usesRequiredPeripheral == FALSE,]
       dataToPresent = dplyr::summarise(dplyr::group_by(dataToPresent, "Publisher" = publisher), "Games Made Backwards Compatible" = length(isBCCompatible[isBCCompatible==TRUE]), "Games Published" = length(gameName), "Percent" = as.integer(round(length(isBCCompatible[isBCCompatible==TRUE])/length(gameName)*100,0)))
       dataToPresent = dplyr::arrange(dataToPresent, Percent, desc(dataToPresent$"Games Published"))
-      dataToPresent[dataToPresent == TRUE] = "Yes"
-      dataToPresent[dataToPresent == FALSE] = "No"
+      dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+      dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
       dataToPresent
     }, 
     n = max(length(dataToPresent$"Percent"[dataToPresent$"Percent" == 0]),25))
@@ -321,8 +321,8 @@ shinyServer(function(input, output, session) {
       dataToPresent = dataToPresent[dataToPresent$isKinectRequired == FALSE & dataToPresent$usesRequiredPeripheral == FALSE,]
       dataToPresent = dplyr::summarise(dplyr::group_by(dataToPresent, "Publisher" = publisher), "Games Made Backwards Compatible" = length(isBCCompatible[isBCCompatible==TRUE]), "Games Published" = length(gameName), "Percent" = as.integer(round(length(isBCCompatible[isBCCompatible==TRUE])/length(gameName)*100,0)))
       dataToPresent = dplyr::arrange(dataToPresent, desc(Percent), desc(dataToPresent$"Games Published"))
-      dataToPresent[dataToPresent == TRUE] = "Yes"
-      dataToPresent[dataToPresent == FALSE] = "No"
+      dataToPresent[is.logical(dataToPresent) & dataToPresent == TRUE] = "Yes"
+      dataToPresent[is.logical(dataToPresent) & dataToPresent == FALSE] = "No"
       dataToPresent
     }, 
     n = 25)
